9d866994c8184254fb4774aa82c77fd1db4004e0
KazuGod <kazuzmafii@gmail.com>
2019-09-18 20:25:25 +0200

    Small script improvements.

--- js/calc.js
+++ js/calc.js
@@ -2,8 +2,18 @@ function calc()
 {
 	var height = document.getElementById('hgtb').value;
 	var weight = document.getElementById('wgtb').value;
-	var result = weight/weight * weight;
+	var height2 = height * height
+	var result = weight/height2;
 
-	document.getElementById("result").innerHTML = result;
+	if(height == 0 || weight == 0)
+	{
+		document.getElementById("result").style.marginRight = ''
+		document.getElementById("result").innerHTML = "Wrong value!"
+	}
+	else
+	{
+		document.getElementById("result").innerHTML = result;
+	}
+
 
 }
